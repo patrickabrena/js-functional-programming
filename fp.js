6b85a1a9e713df64a9202c4aea91810fde5bce4e
Patrick Abrena <patrickabrena@hotmail.com>
2024-03-13 15:36:30 -0500

    finish up to and inlcuding 'USE_THE_FILTER_METHOD_TO_EXTRACT_DATA_FROM_AN_ARRAY'

--- fp.js
+++ fp.js
@@ -568,3 +568,223 @@ const USE_THE_MAP_METHOD_TO_EXTRACT_DATA_FROM_AN_ARRAY = () => {
   console.log(JSON.stringify(ratings));
 };
 // USE_THE_MAP_METHOD_TO_EXTRACT_DATA_FROM_AN_ARRAY();
+
+const IMPLEMENT_MAP_ON_A_PROTOTYPE = () => {
+  // As I have seeen from applying Array.prototype.map() or just map(), the map method returns an arraay of the same length as the one it was called on. It also DOESN'T alter thee original array, as long as its callbaack function doesn't.
+  //
+  // In other words, map is a pure function, and its output depends solely on its inputs. Plus, it takes another function as its argument.
+  //
+  // You might leearn a lot about the map method if you implement your own version of it. It is recommended you use a for loop or Array.prototype.forEach()
+  //
+  /*****/
+  //
+  // Write your own Array.prototype.myMap() which should behave exactly like Array.prototype.map(). You should not use the built-in map method. The array instance can be accessed in the myMap method using "this" keyword
+  /*
+  Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+
+  // Only change code above this line
+  return newArray;
+};
+  */
+  Array.prototype.myMap = function (callback) {
+    // using prototype prop on "Array" object so thaat the myMap method can be accessed across all instances of "Array"
+    const newArray = [];
+    // Only change code below this line
+    //
+    // start by making a for loop anad using "this" keyword which REMEMBER: is a reference to the object that the method it's associated with
+    for (let index = 0; index < this.length; index += 1) {
+      // going to run "callback" function on newArray
+      // pushing everything in "this" through the callback function INTO the newArray
+      // ALSO remember map() method syntax:
+      // - map(callbackFn)
+      // - map(callbackFn, thisArg)
+      // Parameters IN ORDER
+      // element - the current element being processed in the array
+      // index - the index of the current element being processed in the array
+      // array - the array "map()" was called upon
+      newArray.push(callback(this[index], index, this));
+      // this[index] - element
+      // index - index
+      // this - this
+    } // Only change code above this line
+    return newArray;
+  };
+  let array = [1, 2, 3, 4, 5];
+  // creating test case instances of Array
+  let doubled = array.myMap((element) => element * 2);
+  let frontToBack = array.myMap(
+    (_, index, arr) => array[index + 1] || array[0]
+  );
+  console.log(doubled, frontToBack);
+};
+// IMPLEMENT_MAP_ON_A_PROTOTYPE();
+
+const USE_THE_FILTER_METHOD_TO_EXTRACT_DATA_FROM_AN_ARRAY = () => {
+  // ANother useful array function is "Array.prototype.filter()" or just "filter()"
+  //
+  // "filter" calls a function on each element of an array and returns a new array containing ONLY the elements for which that function returns a TRUTHY value
+  //
+  // just like map, "filter" does not modify the original array
+  //
+  // the callback function accepts 3 arguments
+  // ARGS IN ORDER
+  // element - current element being processed in the array
+  // index - index of that element
+  // array - the array upon which fiilter method was called
+  const EX = () => {
+    //
+    const users = [
+      { name: "John", age: 34 },
+      { name: "Amy", age: 20 },
+      { name: "camperCat", age: 10 },
+    ];
+
+    const usersUnder30 = users.filter((user) => user.age < 30);
+    // console.log(usersUnder30); // will log  [ { name: 'Amy', age: 20 }, { name: 'camperCat', age: 10 } ]
+    //
+    /******/
+    //
+    // The variable watchList holds an array of objects with information on several movies. Use a combination of filter and map on watchlist to assign a new array of objects with only title and rating keys.
+    // The new array should only include objects where imdbRating is greater than or equal to 8.0.
+    // Note: the rating values are saved as strings in the objects and you may need to convert them into numbers to perform mathematical operations on them
+    // The global variable
+    const watchList = [
+      {
+        Title: "Inception",
+        Year: "2010",
+        Rated: "PG-13",
+        Released: "16 Jul 2010",
+        Runtime: "148 min",
+        Genre: "Action, Adventure, Crime",
+        Director: "Christopher Nolan",
+        Writer: "Christopher Nolan",
+        Actors:
+          "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+        Plot: "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+        Language: "English, Japanese, French",
+        Country: "USA, UK",
+        Awards: "Won 4 Oscars. Another 143 wins & 198 nominations.",
+        Poster:
+          "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+        Metascore: "74",
+        imdbRating: "8.8",
+        imdbVotes: "1,446,708",
+        imdbID: "tt1375666",
+        Type: "movie",
+        Response: "True",
+      },
+      {
+        Title: "Interstellar",
+        Year: "2014",
+        Rated: "PG-13",
+        Released: "07 Nov 2014",
+        Runtime: "169 min",
+        Genre: "Adventure, Drama, Sci-Fi",
+        Director: "Christopher Nolan",
+        Writer: "Jonathan Nolan, Christopher Nolan",
+        Actors:
+          "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+        Plot: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+        Language: "English",
+        Country: "USA, UK",
+        Awards: "Won 1 Oscar. Another 39 wins & 132 nominations.",
+        Poster:
+          "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+        Metascore: "74",
+        imdbRating: "8.6",
+        imdbVotes: "910,366",
+        imdbID: "tt0816692",
+        Type: "movie",
+        Response: "True",
+      },
+      {
+        Title: "The Dark Knight",
+        Year: "2008",
+        Rated: "PG-13",
+        Released: "18 Jul 2008",
+        Runtime: "152 min",
+        Genre: "Action, Adventure, Crime",
+        Director: "Christopher Nolan",
+        Writer:
+          "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+        Actors: "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+        Plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+        Language: "English, Mandarin",
+        Country: "USA, UK",
+        Awards: "Won 2 Oscars. Another 146 wins & 142 nominations.",
+        Poster:
+          "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+        Metascore: "82",
+        imdbRating: "9.0",
+        imdbVotes: "1,652,832",
+        imdbID: "tt0468569",
+        Type: "movie",
+        Response: "True",
+      },
+      {
+        Title: "Batman Begins",
+        Year: "2005",
+        Rated: "PG-13",
+        Released: "15 Jun 2005",
+        Runtime: "140 min",
+        Genre: "Action, Adventure",
+        Director: "Christopher Nolan",
+        Writer:
+          "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+        Actors: "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+        Plot: "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+        Language: "English, Urdu, Mandarin",
+        Country: "USA, UK",
+        Awards: "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+        Poster:
+          "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+        Metascore: "70",
+        imdbRating: "8.3",
+        imdbVotes: "972,584",
+        imdbID: "tt0372784",
+        Type: "movie",
+        Response: "True",
+      },
+      {
+        Title: "Avatar",
+        Year: "2009",
+        Rated: "PG-13",
+        Released: "18 Dec 2009",
+        Runtime: "162 min",
+        Genre: "Action, Adventure, Fantasy",
+        Director: "James Cameron",
+        Writer: "James Cameron",
+        Actors: "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+        Plot: "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+        Language: "English, Spanish",
+        Country: "USA, UK",
+        Awards: "Won 3 Oscars. Another 80 wins & 121 nominations.",
+        Poster:
+          "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+        Metascore: "83",
+        imdbRating: "7.9",
+        imdbVotes: "876,575",
+        imdbID: "tt0499549",
+        Type: "movie",
+        Response: "True",
+      },
+    ];
+
+    // Only change code below this line
+    // set up just like the previous watchList example with map method
+    const filteredList = watchList
+      .map((movies) => ({
+        title: movies["Title"],
+        rating: movies["imdbRating"],
+      }))
+      .filter((movies) => movies.rating >= 8.0);
+    // need to do map method then chain filter method after otherwise I'll get an error if I try to do const filteredList = watchList.filter(...)
+    // Only change code above this line
+
+    console.log(filteredList);
+  };
+  EX();
+};
+USE_THE_FILTER_METHOD_TO_EXTRACT_DATA_FROM_AN_ARRAY();
